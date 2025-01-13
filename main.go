337d4c315a7b9491d5ba65f7ccb5028f9f56bc82
Suryakant das <dassuryakantadas@gmail.com>
2025-01-13 23:55:17 +0530

    coin collect functionality added and level inscrease fuctionality

--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"math/rand"
 
 	"github.com/gdamore/tcell/v2"
 )
 
+func drawString(screen tcell.Screen, x, y int, msg string) {
+	for index, char := range msg {
+		screen.SetContent(x+index, y, char, nil, tcell.StyleDefault)
+	}
+
+}
+
+func setupCoin(level int) []*Sprite {
+	coins := make([]*Sprite, level+2)
+	for index := range level + 2 {
+		coins[index] = NewSprite('0', rand.Intn(20), rand.Intn(20))
+	}
+	return coins
+}
 func main() {
 	screen, err := tcell.NewScreen()
 
@@ -21,11 +37,16 @@ func main() {
 	}
 
 	player := NewSprite('@', 10, 10)
-	coins := []*Sprite{
-		NewSprite('0', 10, 26),
-		NewSprite('0', 40, 10),
-		NewSprite('0', 15, 53),
-	}
+	// coins := []*Sprite{
+	// 	NewSprite('0', 10, 26),
+	// 	NewSprite('0', 40, 10),
+	// 	NewSprite('0', 15, 53),
+	// }
+	coins := setupCoin(1)
+
+	score := 0
+	level := 1
+
 	//game loop-while running , update state,draw screen ,else close
 	running := true
 	for running {
@@ -36,6 +57,18 @@ func main() {
 		for _, coin := range coins {
 			coin.Draw(screen)
 		}
+
+		// ui
+
+		drawString(
+			screen,
+			1, 1, fmt.Sprint("Score: ", score),
+		)
+		drawString(
+			screen,
+			1, 2, fmt.Sprint("level: ", level),
+		)
+
 		screen.Show()
 
 		// running logic
@@ -43,7 +76,7 @@ func main() {
 		event := screen.PollEvent()
 
 		// checking the event type
-
+		playerMoved := false
 		switch event := event.(type) {
 		case *tcell.EventKey:
 			switch event.Rune() {
@@ -51,14 +84,42 @@ func main() {
 				running = false
 			case '8':
 				player.y -= 1
+				playerMoved = true
 			case '2':
 				player.y += 1
+				playerMoved = true
+
 			case '4':
 				player.x -= 1
+				playerMoved = true
+
 			case '6':
 				player.x += 1
+				playerMoved = true
 
 			}
+		}
+		if playerMoved {
+			coinCollectedIndex := -1
+			for index, coin := range coins {
+				if coin.x == player.x && coin.y == player.y {
+					coinCollectedIndex = index
+					score++
+				}
+			}
+
+			if coinCollectedIndex > -1 {
+				coins[coinCollectedIndex] = coins[len(coins)-1]
+				coins = coins[0 : len(coins)-1]
+
+				if len(coins) == 0 {
+					level++
+					coins = setupCoin(level)
+				}
+			}
+
+			// 0 1 2 3 4
+			// 0 1 4 3 4
 		}
 	}
 
